372397dc52a8b416514e4d827aceffd06ae37311
vagrant <vagrant@vagrant.vm>
2021-03-17 03:43:42 +0000

    created function to take in user input and convert to list

--- calculator.py
+++ calculator.py
@@ -5,3 +5,28 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+##User input 
+def change_user_input_to_list():
+    user_input = input('Enter your equation here: ')
+    user_input_list = user_input.split(' ') 
+    return (user_input_list)
+
+
+#while exit_condition_not_reached:
+#    input = consume_input()
+#    output = evaluate_input(input)
+#    print(output)
+
+
+#repeat forever:
+#    read input
+#    tokenize input
+#        if the first token is "q":
+#            quit
+#        else:
+#            (decide which math function to call based on first token)
+#            if the first token is 'pow':
+#                  call the power function with the other two tokens
+
+ #           (...etc.)
